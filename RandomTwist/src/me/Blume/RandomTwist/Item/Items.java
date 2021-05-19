ee8bf487b3a77d1a687ef8eb833f44289953e6d0
ulasonmez <ulasonmez@outlook.com>
2021-05-19 21:23:16 +0300

    upgrades

--- RandomTwist/src/me/Blume/RandomTwist/Item/Items.java
+++ RandomTwist/src/me/Blume/RandomTwist/Item/Items.java
@@ -1,14 +1,13 @@
 package me.Blume.RandomTwist.Item;
 
-import org.bukkit.Bukkit;
+import java.util.HashMap;
+
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.inventory.CraftingInventory;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -23,36 +22,41 @@ public class Items implements Listener {
 		meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
 		meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
 		Printer.setItemMeta(meta);
-		
+
 	}
+	@SuppressWarnings("serial")
 	@EventHandler
 	public void craftstick(PrepareItemCraftEvent event) {
 		CraftingInventory inv = event.getInventory();
 		if(inv.getMatrix().length<9) return;
-		//012
-		//345
-		//678
+		checkCraft(Items.Printer,inv,new HashMap<Integer,ItemStack>(){{
+			put(0,new ItemStack(Material.IRON_INGOT));
+			put(1,new ItemStack(Material.IRON_INGOT));
+			put(2,new ItemStack(Material.IRON_INGOT));
+			put(3,new ItemStack(Material.IRON_INGOT));
+			put(4,new ItemStack(Material.STICK));
+			put(5,new ItemStack(Material.IRON_INGOT));
+			put(6,new ItemStack(Material.IRON_INGOT));
+			put(7,new ItemStack(Material.STICK));
+			put(8,new ItemStack(Material.IRON_INGOT));
+		}});
+
+
+	}
+	public void checkCraft(ItemStack result,CraftingInventory inv,HashMap<Integer, ItemStack> ingredients) {
 		ItemStack[] matrix = inv.getMatrix();
-			if(matrix[0]!=null && matrix[1]!=null && matrix[2]!=null &&
-					matrix[3]!=null && matrix[4]!=null && matrix[5]!=null &&
-					matrix[6]!=null && matrix[7]!=null && matrix[8]!=null ) {
-				boolean stickshape=true;
-				for(int i =0;i<9;i++) {
-				if(i==4 || i==7) {
-					if(!(matrix[i].getType().equals(Material.STICK))) stickshape=false;
-				}
-				else {
-					if(!(matrix[i].getType().equals(Material.IRON_INGOT))) stickshape = false;
+		for(int i =0;i<9;i++) {
+			if(ingredients.containsKey(i)) {
+				if(matrix[i]==null || !matrix[i].equals(ingredients.get(i))) {
+					return;
 				}
 			}
-				if(stickshape) {
-					if(Items.randomtwistkomut==1) {
-						inv.setResult(Items.Printer);
-					}
-					else return;
+			else {
+				if(matrix[i]!=null) {
+					return;
 				}
 			}
-		
-		
-}
+		}
+			inv.setResult(result);
+	}
 }
